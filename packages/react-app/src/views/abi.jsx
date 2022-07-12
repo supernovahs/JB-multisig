a3503ba1807fee61a365ffde21ebfdf4a69182f5
supernovahs <harshitsinghal252@gmail.com>
2022-07-12 20:06:11 +0530

    Balance of project

---
+++ packages/react-app/src/views/abi.jsx
@@ -0,0 +1,7 @@
+/// How to get a Balance of a particular project ?
+
+// Contract Name  JBSingleTokenPaymentTerminalStore  https://rinkeby.etherscan.io/address/0x5d4eb71749dd9984118ebdf96aaf3cf6eae1a745#code
+// function Name balanceOf(address , uint )
+// address:  Pass the address of the JBETH payment terminal contract 0x765A8b9a23F58Db6c8849315C04ACf32b2D55cF8
+// uint:  pass the project id
+// Ex  balancOf(0x765A8b9a23F58Db6c8849315C04ACf32b2D55cF8 , 4266);
